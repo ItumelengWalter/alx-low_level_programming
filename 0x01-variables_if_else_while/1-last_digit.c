29f06350932ed5e685987fe788eb9bd5ce5a9613
Itumeleng Walterkekana <itumelengwalter14@gmail.com>
2023-02-17 05:45:29 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	if (a > 5)
+	{
+		printf("Last digit of %d os %d and is grester thsn5\n", n, a);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+	}
+	return (0);
+}
+
