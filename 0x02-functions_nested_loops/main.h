380ae825df38c4dd308632946344ff4faf76c882
Itumeleng Walterkekana <itumelengwalter14@gmail.com>
2023-02-21 08:21:01 -0800

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabe_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int a);
-int print_last_digit(int num);
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
-int add(int num1, int num2);
+void times_table(void);
+int add(int, int);
 void print_to_98(int n);
-
-#endif
+void print_time_table(int n);
