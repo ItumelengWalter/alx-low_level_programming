87639e1147a4593f039aab20d2c12f126bc0e8c4
Itumeleng Walterkekana <itumelengwalter14@gmail.com>
2023-02-21 07:34:25 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print alphabet (void);
+void print_alphabet(void);
+void print_alphabe_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
