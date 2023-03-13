cc585887cf82109d31d728b17d99525d1eab6b7b
Itumeleng Walterkekana <itumelengwalter14@gmail.com>
2023-03-13 12:36:04 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,6 +25,7 @@ char *str_concat(char *s1, char *s2)
 
 	while (s2[j])
 		j++;
+
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 
@@ -36,7 +37,8 @@ char *str_concat(char *s1, char *s2)
 	while (k < l)
 	{
 		if (k <= i)
-			s[k] = s2[k];
+			s[k] = s1[k];
+
 		if (k >= i)
 		{
 			s[k] = s2[j];
