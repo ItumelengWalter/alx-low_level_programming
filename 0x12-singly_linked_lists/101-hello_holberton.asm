c78452d9a0f70eef65c3f509dc7660fb3658af38
Itumeleng Walterkekana <itumelengwalter14@gmail.com>
2023-03-30 04:53:32 -0700

    Advanced Task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+global 	main
+	extern 	printf
+
+	section .text
+main:
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
